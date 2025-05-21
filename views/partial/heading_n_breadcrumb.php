c3d258b553211cad52bec3ab00124e75879bd78f
typingmonk <tmonk.hsieh@tmonk.tw>
2025-05-21 12:28:05 +0800

    Adjust margin and padding in heading_n_breadcrumb

--- views/partial/heading_n_breadcrumb.php
+++ views/partial/heading_n_breadcrumb.php
@@ -1,12 +1,12 @@
 <!-- content-header -->
-<div class="content-header">
+<div class="content-header pb-0">
   <div class="container-fluid">
     <div class="row">
       <div class="col-sm-6">
        <h1><?= $this->escape($this->heading) ?></h1>
       </div>
     </div>
-    <div class="row mt-2">
+    <div class="row">
       <div class="col-sm-6">
         <!-- breadcrumb -->
         <ol class="breadcrumb">
