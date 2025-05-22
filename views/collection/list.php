ae5a510b9c5093c09a1f67237ee7b45486bb41f4
typingmonk <tmonk.hsieh@tmonk.tw>
2025-05-22 16:02:19 +0800

    Fix indention

--- views/collection/list.php
+++ views/collection/list.php
@@ -4,15 +4,15 @@ $this->heading = $this->escape($config['name']) . ' / ' . $this->escape($this->t
 ?>
 <?php $this->yield_start('content') ?>
 <ul class="nav nav-pills my-3">
-    <?php if (count($this->features) > 1) { ?>
-      <?php foreach ($this->features as $ftab => $fname) { ?>
-      <li class="nav-item">
-        <a class="nav-link <?= $this->if($ftab == $this->tab, 'active') ?>" href="/collection/list/<?= $this->type ?>/<?= $ftab ?>">
-          <?= $this->escape($fname) ?>
-        </a>
-      </li>
-      <?php } ?>
+  <?php if (count($this->features) > 1) { ?>
+    <?php foreach ($this->features as $ftab => $fname) { ?>
+    <li class="nav-item">
+      <a class="nav-link <?= $this->if($ftab == $this->tab, 'active') ?>" href="/collection/list/<?= $this->type ?>/<?= $ftab ?>">
+        <?= $this->escape($fname) ?>
+      </a>
+    </li>
     <?php } ?>
+  <?php } ?>
 </ul>
 <?php if ($this->tab == 'table') { ?>
 <?= $this->partial('collection/table', $this) ?>
