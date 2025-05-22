8cd1765f2f5bbef61312f082904c78b8ef69ee24
typingmonk <tmonk.hsieh@tmonk.tw>
2025-05-22 16:08:28 +0800

    Make heading and breadcrumb in different row

--- views/partial/heading_n_breadcrumb.php
+++ views/partial/heading_n_breadcrumb.php
@@ -2,12 +2,12 @@
 <div class="content-header pb-0">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-6">
+      <div class="col-12">
        <h1><?= $this->escape($this->heading) ?></h1>
       </div>
     </div>
     <div class="row">
-      <div class="col-sm-6">
+      <div class="col-12">
         <!-- breadcrumb -->
         <ol class="breadcrumb">
         </ol>
